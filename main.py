684afb9874307b044c1a2f846906dff3efa9afc2
Jack <yoshifan509@gmail.com>
2018-11-11 05:25:12 -0600

    init

---
+++ main.py
@@ -0,0 +1,88 @@
+import shawk
+import json
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from threading import Thread
+from time import sleep
+import requests
+
+with open('config.json') as f:
+    config = json.load(f)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///catfacts.db'
+db = SQLAlchemy(app)
+
+def call_at_interval(period, callback, args):
+    while True:
+        sleep(period)
+        callback(*args)
+
+def setInterval(period, callback, *args):
+    Thread(target=call_at_interval, args=(period, callback, args)).start()
+
+class Users(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    phone = db.Column(db.String(100))
+    network = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+
+db.create_all()
+
+user = "thesmscatfacts@gmail.com"
+password = config["password"]
+client = shawk.Client(user, password)
+
+
+def firstMsg(num, name):
+    user = Users.query.filter_by(phone=num)
+    client.send("Hey {0}! An anonymous friend signed you up for daily CatFacts! Every day you will recieve interesting cat related facts! *If you would like to unsubscribe, please visit this link: http://0.0.0.0/stop/{1}".format(name, num), num) # replace link with domain
+
+def dailymsg(phone):
+    fact = requests.get("https://catfact.ninja/fact")
+    fact = fact.json()
+    user = Users.query.filter_by(phone=phone)
+    client.send("Your daily catfact is here! FACT: {}".format(fact["fact"]), user.phone)
+
+
+def iterate():
+    for user in Users.query.all():
+        dailymsg(user.phone)
+
+
+
+# setInterval(86400, iterate)
+
+setInterval(10, iterate)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        phone = request.form.get("phone")
+        network = request.form.get("network")
+        name = request.form.get("name")
+        tocommit = Users(id=len(Users.query.all()), phone=phone, network=network, name=name)
+        db.session.add(tocommit)
+        db.session.commit()
+        client.add_contact(phone, network, name)
+        firstMsg(phone, name)
+        return redirect("/")
+    return render_template("signup.html")
+
+@app.route("/stop/<phone>")
+def stop(phone):
+    todelete = Users.query.filter_by(phone=phone)
+    db.session.delete(todelete)
+    db.session.commit()
+    client.remove_contact(number=phone)
+    return render_template("stop.html", msg="Success!")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="80", debug=True)
+
+
