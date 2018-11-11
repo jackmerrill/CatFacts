1eeda2877b909e977a15fe748141b73095653bf8
Jack <yoshifan509@gmail.com>
2018-11-11 08:55:26 -0600

    bug fix

---
+++ gateways.py
@@ -0,0 +1,538 @@
+Gateways = {
+    {
+        "name": 'President\'s Choice',
+        "email": 'txt.bell.ca'
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+        {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+        {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+        {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+    {
+        "name": ,
+        "email": 
+    },
+}
